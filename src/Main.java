eab12291d93323fb36e0f2953edd9d33e397b509
Maxim Nebo <gp.max.dev@gmail.com>
2023-01-04 20:37:53 +0200

    subtusk4 вроде все работает, надо перепроверить еще все функции и пройтись по всем методам которые могут выкинуть java.util.NoSuchElementException

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-       String testString = "priposvit positregposposa";
-        //System.out.println(testString.replaceFirst("it", "11"));
+       //String testString = "priposvit positregposposa";
+       // System.out.println(Runtime.getRuntime().availableProcessors());
         //System.out.println(testString.indexOf("pos"));
         //System.out.println(testString.contains("pos"));
         //testString.contains("pos");
-        System.out.println(SubTask2.substringCount("pripopspovpiptpppppp","po",1, 20));
+       // System.out.println(SubTask2.substringCount("pripopspovpiptpppppp","po",1, 20));
        //System.out.println(testString.substring(3));
 
 
--- src/ua/com/sourceit/nebotov/subtusk4/ArrayImpl.java
+++ src/ua/com/sourceit/nebotov/subtusk4/ArrayImpl.java
@@ -1,86 +1,140 @@
 package ua.com.sourceit.nebotov.subtusk4;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.function.Consumer;
+
+
 
 public class ArrayImpl  implements Array {
 
-
-
-
-    class IteratorImpl implements Iterator {
-
-
-        @Override
-        public boolean hasNext() {
-            return false;
-        }
-
-        @Override
-        public Object next() {
-            return null;
-        }
-
-        @Override
-        public void remove() {
-            Iterator.super.remove();
-        }
-
-        @Override
-        public void forEachRemaining(Consumer action) {
-            Iterator.super.forEachRemaining(action);
-        }
-    }
-
-
+    ArrayList <Object> objectArrayList = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        ArrayImpl arrayImplements = new ArrayImpl();
+        arrayImplements.add("A");
+        arrayImplements.add("b");
+        arrayImplements.add("c");
+
+        arrayImplements.set(1, "B");
+
+
+        System.out.println(arrayImplements.objectArrayList.get(1));
+        System.out.println(arrayImplements.indexOf("c"));
+        System.out.println(arrayImplements.iterator().hasNext()); // ?? iterator()??
+        System.out.println(arrayImplements.toString());
+
+        arrayImplements.clear();
+        System.out.println(arrayImplements.iterator().hasNext());
+        System.out.println(arrayImplements.toString());
+
     }
 
-
-
-
-
-
+    @Override
+    public String toString() {
+        return objectArrayList + " ";
+    }
 
     @Override
     public void add(Object element) {
+
+        objectArrayList.add(element);
 
     }
 
     @Override
     public void set(int index, Object element) {
 
+        objectArrayList.set(index, element);
+
     }
 
     @Override
     public Object get(int index) {
+
+        if(objectArrayList.size() >= index) {
+            return objectArrayList.get(index);
+
+        }
         return null;
     }
 
     @Override
     public int indexOf(Object element) {
-        return 0;
+
+        if (!objectArrayList.isEmpty()){
+
+            for(Object arrayObject : objectArrayList){
+
+                if(arrayObject.equals(element)){
+
+                    return objectArrayList.indexOf(arrayObject);
+                }
+            }
+        }
+
+            return -1;
     }
 
     @Override
     public void remove(int index) {
+
+        if (!objectArrayList.isEmpty() && (objectArrayList.size() >= index) ){
+
+            objectArrayList.remove(index);
+        }
 
     }
 
     @Override
     public void clear() {
 
-    }
+        if (!objectArrayList.isEmpty()){
+            objectArrayList.removeAll(objectArrayList);
+        }
+     }
 
     @Override
     public int size() {
+        if (!objectArrayList.isEmpty()){
+            return objectArrayList.size();
+        }
         return 0;
     }
 
     @Override
     public Iterator<Object> iterator() {
-        return null;
+
+        return new IteratorImpl();
+
+    }
+
+    class IteratorImpl implements Iterator {
+
+
+        @Override
+        public boolean hasNext() {
+
+
+                return objectArrayList.iterator().hasNext();
+        }
+
+        @Override
+        public Object next() {
+
+            if (hasNext()){
+              objectArrayList.iterator().next();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void remove() {
+            this.remove();
+            //Iterator.super.remove();
+        }
+
+
     }
 }
